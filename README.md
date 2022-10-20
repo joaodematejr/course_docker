997ae00169e348e1771ddc290ba541d1591ea1a4
joaodematejr <joaodematejr@gmail.com>
2022-10-19 23:28:53 -0300

    comandos basico containers

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Docker para Desenvolvedores (com Docker Swarm e Kubernetes)
 
 
 Aprenda Docker do básico ao avançado e ainda orquestração com Docker Swarm e Kubernetes!
+
+
+Link: https://github.com/matheusbattisti/curso_docker
---
+++ command.c
@@ -0,0 +1,28 @@
+docker run docker/whalesay cowsay hello_world
+
+docker run ubuntu
+
+docker run -it ubuntu
+
+docker run -it node
+
+docker ps
+
+docker  ps -a
+
+docker run nginx 
+
+docker run -d nginx 
+
+docker run -d -p <port>:<port> nginx 
+
+docker run -d -p <port_person>:<port> nginx 
+
+docker start id
+
+docker run -d -p <port_person>:<port> --name <name_img> nginx 
+
+docker logs <id>
+
+docker rm <id> or docker rm <id> -f
+
