b3547326dd550d9c224f7d5efb69feb299c25ed1
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-10-26 11:05:37 -0300

    command bind

--- php/command.ps1
+++ php/command.ps1
@@ -3,3 +3,5 @@ docker build -t phpmessages .
 docker run -d -p 8080:80 --name phpmessages phpmessages
 
 docker run -d -p 80:80 --name phpmessages_container -v phpvolume:/var/www/html/messages --rm phpmessages
+
+docker run -d -p 80:80 --name phpmessages_container -v /Users/frameticket/Documents/github/course_docker/php/messages:/var/www/html/messages --rm phpmessages
