267211c83a5d31060b358657f03fb181a6734806
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-10-26 11:36:40 -0300

    bind mount

--- php/command.ps1
+++ php/command.ps1
@@ -5,3 +5,6 @@ docker run -d -p 8080:80 --name phpmessages phpmessages
 docker run -d -p 80:80 --name phpmessages_container -v phpvolume:/var/www/html/messages --rm phpmessages
 
 docker run -d -p 80:80 --name phpmessages_container -v /Users/frameticket/Documents/github/course_docker/php/messages:/var/www/html/messages --rm phpmessages
+
+bind mount (importante) 
+docker run -d -p 80:80 --name phpmessages_container -v /Users/frameticket/Documents/github/course_docker/php:/var/www/html/ --rm phpmessages
--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <h1>Escreva sua Mensagem! BIND MOUNT</h1>
+    <h1>Escreva sua Mensagem! BIND MOUNT 2</h1>
     <form action="process.php" method="POST">
         <input type="text" name="message" id="message">
         <input type="submit" value="Enviar Mensagem">
