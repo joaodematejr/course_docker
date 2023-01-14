7923380ded7a7b8bcdd114aee1a4a1e42cac6f80
joaodematejr <joaodematejr@gmail.com>
2023-01-13 23:11:51 -0300

    command kubernetes

--- kubernetes/cmd.ps1
+++ kubernetes/cmd.ps1
@@ -4,3 +4,27 @@ choco install minikube
 minikube start --driver=docker
 
 minikube start --force --driver=docker
+
+minikube stop
+
+minikube dashboard
+
+minikube dashboard --url
+
+docker build -t joaodematejr/flask-hub-projeto .
+
+docker run -d -p 5000:5000 --name flask-hub --rm joaodematejr/flask-hub-projeto
+
+docker push joaodematejr/flask-hub-projeto
+
+kubectl create deployment flask-deployment --image=joaodematejr/flask-hub-projeto
+
+kubectl get deployments
+
+kubectl describe deployment
+
+kubectl get pods
+
+kubectl describe pod
+
+kubectl config view
